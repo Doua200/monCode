018ac0752efccd9aeba08faeda76ad4011fe11d9
Doua20 <douakona@gmail.com>
2024-02-28 09:13:33 +0100

    mercredi-28fev24

---
+++ php/app/mestp/associations/cas-livre/tp-asso/realisateur.php
@@ -0,0 +1,11 @@
+<?php
+//association/realisateur.php
+class Realisateur {
+    public $nom;
+    public $prenom;
+
+    public function __construct($nom, $prenom) {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+    }
+}
